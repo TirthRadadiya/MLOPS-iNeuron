d94c7ac02f401741151dc0bf1ae94ef0e75d3843
TirthRadadiya <radadiyatirth@gmail.com>
2023-11-09 20:52:08 +0530

    exception and logging added and model training notebook implmented

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
+# refrence repo - https://github.com/krishnaik06/FSDSRegression/tree/main
+
 from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
 
 setup(
-    name="census-income",
+    name="Diamond Price Predictor",
     version="0.0.1",
     author="Tirth",
     author_email="radadiyatirth@gmail.com",
     packages=find_packages(),
-    install_requires=["pandas", "numpy", "flask"],
+    install_requires=get_requirements("requirements.txt"),
 )
